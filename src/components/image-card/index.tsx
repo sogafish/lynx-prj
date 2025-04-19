eecb3334944c1b8b717dea310f549324ecbc917d
sogafish <kent.soga@gmail.com>
2025-04-19 13:32:55 +0900

    FirstComponent

---
+++ src/components/image-card/index.tsx
@@ -0,0 +1,14 @@
+import type { FC } from "react";
+// import "./styles.scss";
+
+type Props = {
+  // picture: string;
+};
+
+export const ImageCard: FC<Props> = () => {
+  return (
+    <view>
+      <text>Test</text>
+    </view>
+  );
+};
