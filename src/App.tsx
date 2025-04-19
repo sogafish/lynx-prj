7d5ae754a5b87fb39c27b4f7c55cd081cb8a0df0
sogafish <kent.soga@gmail.com>
2025-04-19 13:32:59 +0900

    tmp

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,50 @@
-import { useCallback, useEffect, useState } from '@lynx-js/react'
+import { useCallback, useEffect, useState } from "@lynx-js/react";
 
-import './App.css'
-import arrow from './assets/arrow.png'
-import lynxLogo from './assets/lynx-logo.png'
-import reactLynxLogo from './assets/react-logo.png'
+import "./App.css";
+import arrow from "./assets/arrow.png";
+import lynxLogo from "./assets/lynx-logo.png";
+import reactLynxLogo from "./assets/react-logo.png";
+import { ImageCard } from "./components/image-card/index.jsx";
 
 export function App() {
-  const [alterLogo, setAlterLogo] = useState(false)
+  const [alterLogo, setAlterLogo] = useState(false);
 
   useEffect(() => {
-    console.info('Hello, ReactLynx')
-  }, [])
+    console.info("Hello, ReactLynx");
+  }, []);
 
   const onTap = useCallback(() => {
-    'background only'
-    setAlterLogo(!alterLogo)
-  }, [alterLogo])
+    "background only";
+    setAlterLogo(!alterLogo);
+  }, [alterLogo]);
 
   return (
     <view>
-      <view className='Background' />
-      <view className='App'>
-        <view className='Banner'>
-          <view className='Logo' bindtap={onTap}>
-            {alterLogo
-              ? <image src={reactLynxLogo} className='Logo--react' />
-              : <image src={lynxLogo} className='Logo--lynx' />}
+      <view className="Background" />
+      <view className="App">
+        <ImageCard />
+
+        {/* <view className="Banner">
+          <view className="Logo" bindtap={onTap}>
+            {alterLogo ? (
+              <image src={reactLynxLogo} className="Logo--react" />
+            ) : (
+              <image src={lynxLogo} className="Logo--lynx" />
+            )}
           </view>
-          <text className='Title'>React</text>
-          <text className='Subtitle'>on Lynx</text>
+          <text className="Title">React</text>
+          <text className="Subtitle">on Lynx</text>
         </view>
-        <view className='Content'>
-          <image src={arrow} className='Arrow' />
-          <text className='Description'>Tap the logo and have fun!</text>
-          <text className='Hint'>
-            Edit<text style={{ fontStyle: 'italic' }}>{' src/App.tsx '}</text>
+        <view className="Content">
+          <image src={arrow} className="Arrow" />
+          <text className="Description">Tap the logo and have fun!</text>
+          <text className="Hint">
+            Edit<text style={{ fontStyle: "italic" }}>{" src/App.tsx "}</text>
             to see updates!
           </text>
-        </view>
-        <view style={{ flex: 1 }}></view>
+        </view> */}
+        {/* <view style={{ flex: 1 }}></view> */}
       </view>
     </view>
-  )
+  );
 }
